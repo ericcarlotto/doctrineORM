18df30516b18f628e779eaaaf1cbb2eddb2fee68
DESKTOP-4TQN1EG\Eric <ecarlotto@intellipos.com.br>
2021-08-31 15:28:10 -0300

    Uploading the first piece of the project.

---
+++ commands/create-student.php
@@ -0,0 +1,16 @@
+<?php
+
+use Alura\Doctrine\Entity\Student;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$student = new Student();
+$student->setName('Manoel Morand');
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$entityManager->persist($student);
+
+$entityManager->flush();
